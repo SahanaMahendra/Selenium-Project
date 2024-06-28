e28de2ed486729c91ff25690f077185e522fcd17
“SahanaMahendra” <sahanamahendra1996@gmail.com>
2024-06-28 16:24:44 -0400

    Intial checkin

---
+++ PricelineAutomation/src/main/java/base/BaseTest.java
@@ -0,0 +1,41 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class BaseTest {
+ public static WebDriver driver;
+ public static Properties prop;
+
+ public BaseTest(){
+  try {
+   prop = new Properties();
+   FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/com/crm"
+           + "/qa/config/config.properties");
+   prop.load(ip);
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+ }
+
+
+ public static void initialization()
+ {
+  String browserName = prop.getProperty("browser");
+  if(browserName.equals("chrome")){
+   System.setProperty("webdriver.chrome.driver", "/Users/sahanam/Desktop/chromedriver-mac-arm64/chromedriver");
+   driver = new ChromeDriver();
+  }
+  driver.manage().window().maximize();
+  driver.manage().deleteAllCookies();
+  driver.get(prop.getProperty("url"));
+ }
+
+}
